e9eb6752466b20309d2fe5553cae272a661c2f1c
dsundar <sdharmar@encoress.com>
2018-08-10 18:13:50 +0530

    - implemented the kiip Native Reward feature.

---
+++ Kiip/KPKiipBannerCustomEvent.h
@@ -0,0 +1,20 @@
+//
+//  KPKiipBannerCustomEvent.h
+//  MoPubSampleApp
+//
+//  Created by encore on 10/08/18.
+//  Copyright © 2018 MoPub. All rights reserved.
+//
+
+#if __has_include(<MoPub/MoPub.h>)
+#import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+#import <MoPubSDKFramework/MoPub.h>
+#else
+#import "MPBannerCustomEvent.h"
+#import "MoPub.h"
+#endif
+
+@interface KPKiipBannerCustomEvent : MPBannerCustomEvent
+
+@end
